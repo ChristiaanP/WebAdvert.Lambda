3f44afa56e83715017a58956a289d1e938e0b822
ChristiaanP <45262749+ChristiaanP@users.noreply.github.com>
2019-05-23 21:47:48 +0200

    Added Elastic search

---
+++ WebAdvert.SearchWorker/ConfigurationHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WebAdvert.SearchWorker
+{
+    public static class ConfigurationHelper
+    {
+        private static IConfiguration _config = null;
+
+        public static IConfiguration Instance
+        {
+            get
+            {
+                if (_config == null)
+                    _config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+                return _config;
+            }
+        }
+    }
+}
---
+++ WebAdvert.SearchWorker/MappingHelper.cs
@@ -0,0 +1,26 @@
+﻿using AdvertApi.Models.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAdvert.SearchWorker
+{
+    public static class MappingHelper
+    {
+        public static AdvertType MapAdvert(AdvertConfirmedMessage message)
+        {
+            var doc = new AdvertType
+            {
+
+                Id = message.Id,
+                Title = message.Title,
+                CreationDateTime = DateTime.Now
+
+            };
+
+            return doc;
+
+        }
+
+    }
+}
